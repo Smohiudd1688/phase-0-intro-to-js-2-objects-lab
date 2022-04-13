a35f42f82770293fe9577f3923830d1480509637
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-04-12 23:01:59 -0700

    commit tree a29a8aa3d232349bda56d3899ca020fad63ee48c

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+const employee = {
+    name : "Jane Doe",
+    streetAddress : "123 ABC Street",
+}
+
+function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    const newEmployee = {...employeeObj};
+    newEmployee[key] = value;
+
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    employeeObj[key] = value;
+
+    return employeeObj;
+}
+
+function deleteFromEmployeeByKey(employeeObj, key) {
+    const newEmployee = {...employeeObj};
+    delete newEmployee[key];
+    
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObj, key) {
+    delete employeeObj[key];
+    
+    return employeeObj;
+}
